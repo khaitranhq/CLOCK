0036c01d78ce29e6e52a8f48d00b6eacddb62ab5
syphuongcuong3 <leoalan5577@gmail.com>
2019-11-13 22:18:46 +0700

    add file

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var clock = document.querySelector('.clock');
+
+var showTime = function(){
+    var currentDate = new Date();
+
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+
+    var morning = true;
+    if (hours >= 12) morning = false;
+
+    if (hours == 24) hours = 0;
+        else 
+            if (hours > 12) hours -= 12;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+
+    var time = hours + ":" + minutes + ":" + seconds;
+    clock.textContent = time;
+}
+
+setInterval(showTime, 1000);
